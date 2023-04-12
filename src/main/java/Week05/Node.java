3901a832c5520ce68ea136c31b1d1723cf9fe16b
Asus <Asus@LAPTOP-7MELLT7N>
2023-04-12 10:31:50 +0800

    LinkedList

---
+++ src/main/java/Week05/Node.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Week05;
+
+/**
+ *
+ * @author Asus
+ */
+public class Node <E>{
+    E element;
+    Node<E> next;
+    
+    public Node(){
+        
+    }
+    
+    public Node(E element){
+        this.element=element;
+    }
+    
+    
+}
